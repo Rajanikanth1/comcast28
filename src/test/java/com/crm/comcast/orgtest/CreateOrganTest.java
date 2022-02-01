c9b098095983148ae5a7e1cb3b45da2980a7ab3d
Rajanikanth1 <rajanikanth355@gmail.com>
2022-02-01 14:52:27 +0530

    project first commit

---
+++ src/test/java/com/crm/comcast/orgtest/CreateOrganTest.java
@@ -0,0 +1,18 @@
+package com.crm.comcast.orgtest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganTest {
+	@Test
+	public void createorganTest()
+	{
+		System.out.println("Hi this is create org test");
+		System.out.println("hi this is for checking");
+	}
+	
+	
+	
+	
+	
+
+}
